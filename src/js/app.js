374bb4868ffec29db897c4e9a3783abdc90421bb
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-10-22 09:49:42 -0500

    render first componst component

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import React from 'react';
+import reactDOM from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello, World</h1>;
+  }
+}
+
+reactDOM.render(<Hello />, document.getElementById('app'));
