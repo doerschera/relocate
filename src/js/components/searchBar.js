6bac9aa41cba10d0553ce98d5be22cbab5c5f40b
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-10-22 20:30:50 -0500

    button

--- src/js/components/searchBar.js
+++ src/js/components/searchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './button.js'
 
 export default class SearchBar extends React.Component {
   render() {
@@ -6,6 +7,7 @@ export default class SearchBar extends React.Component {
       <div class='row'>
         <div class='col s4 offset-s4'>
           <input class='input-field' value='search'></input>
+          <Button name='search' />
         </div>
       </div>
     )
